9fdd9095d7d4d261e65a59b6bbe7cc4a068a5890
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-05-24 17:53:18 +0200

    added js to update food content

--- src/website.js
+++ src/website.js
@@ -55,18 +55,66 @@ tombSweepingFestival.addEventListener("click", updateFestival1);
     midAutumnFestival.addEventListener("click", updateFestival4);
 
 
-
+    function updateFood1() {
+        document.querySelector("#foodTitle").innerHTML = `Dumplings`;
+        document.querySelector("#foodInformation").innerHTML = 
+        `Common Chinese dumplings are generally consist of minced meat and chopped vegetables wrapped into a piece of dough skin. 
+        In China dumplings usually refers to boiled dumplings. The skin can be either thin and elastic or thicker. 
+        There is even a statement that the skin of a dumpling determines the quality of the dumpling.
+        <br>
+        Some people will place a coin or candy inside the dumpling in the hope of obtaining a fortune or having a sweet life, on Chinese New Year's Eve and special family reunions. 
+        Particularly, in Northern China, people generally eat dumplings on the Winter solstice in the hope of a warm winter. 
+        Extended family members may gather together to make dumplings, and it is also eaten at farewells to family members or friends.`;
+    }
     let dumplingFood = document.querySelector("#dumplings");
     dumplingFood.addEventListener("click", updateFood1);
 
+    function updateFood2() {
+        document.querySelector("#foodTitle").innerHTML = `Chao mian`;
+        document.querySelector("#foodInformation").innerHTML = 
+        `Chao mian or <i>Chow mein</i> are Chinese stir-fried noodles with vegetables and sometimes meat or tofu. The name is a romanization of the Taishanese chāu-mèn. 
+        The dish is popular throughout the Chinese diaspora and appears on the menus of most Chinese restaurants abroad.`;
+    }
     let chaomianFood = document.querySelector("#chaomian");
     chaomianFood.addEventListener("click", updateFood2);
 
+    function updateFood3() {
+        document.querySelector("#foodTitle").innerHTML = `Kung Pao Chicken`;
+        document.querySelector("#foodInformation").innerHTML = 
+        `Kung Pao chicken, also transcribed <i>Gong Bao or Kung Po</i>, is a spicy, stir-fried Chinese dish made with cubes of chicken, peanuts, vegetables, and chili peppers. 
+        The classic dish in Sichuan cuisine originated in the Sichuan Province of south-western China. 
+        <br>
+        Although the dish is found throughout China, there are regional variations that are typically less spicy than the Sichuan serving. 
+        Kung Pao chicken is also a staple of Westernized Chinese cuisine.`;
+    }
     let kungPaoChicken = document.querySelector("#kungPao");
     kungPaoChicken.addEventListener("click", updateFood3);
 
+    function updateFood4() {
+        document.querySelector("#foodTitle").innerHTML = `Hotpot`;
+        document.querySelector("#foodInformation").innerHTML = 
+        `Hotpot, also known as soup-food or steamboat, is a cooking method that originates from China, prepared with a simmering pot of soup stock at the dining table,
+         containing a variety of East Asian foodstuffs and ingredients.
+        Typical hot pot ingredients include thinly sliced meat, leaf vegetables, mushrooms, vermicelli, sliced potatoes, bean products, egg dumplings, tofu, and seafood. 
+        <br>
+        Hot pot meals can enhance friendship and unite family members as several people sit around a pot, talking and eating. 
+        The warm atmosphere makes people feel comfortable and relaxed. Hot pot is especially popular in winter and cold places.
+        Due to the high popularity and the unique way of eating, hot pot has already become the major attraction of the tourism industry in some areas, 
+        such as Chongqing and Sichuan.`;
+    }
     let hotpotFood = document.querySelector("#hotpot");
     hotpotFood.addEventListener("click", updateFood4);
 
+    function updateFood5() {
+        document.querySelector("#foodTitle").innerHTML = `Peking Roasted Duck`;
+        document.querySelector("#foodInformation").innerHTML = 
+        `Peking duck is a dish from Beijing (Peking) that has been prepared since the Imperial era. 
+        The meat is characterized by its thin, crisp skin, with authentic versions of the dish serving mostly the skin and little meat, sliced in front of the diners by the cook. 
+        The meat is often eaten with spring onion, cucumber and sweet bean sauce with pancakes rolled around the fillings.
+        <br>
+        Two notable restaurants in Beijing which serve this dish are <i>Quanjude</i> and <i>Bianyifang</i>, 
+        both centuries-old establishments which have become household names, each with their own style:
+        Quanjude is known for using the hung oven roasting method, while Bianyifang uses the oldest technique of closed oven roasting.`;
+    }
     let pekinDuckFood = document.querySelector("#pekingDuck");
     pekinDuckFood.addEventListener("click", updateFood5);
